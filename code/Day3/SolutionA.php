55f8870c8f456d2bd06b4d2818a01821d74cf3c0
dcorto <daviscorto@gmail.com>
2021-12-08 21:45:08 +0100

    day 3 part 1 done

--- code/Day3/SolutionA.php
+++ code/Day3/SolutionA.php
@@ -14,9 +14,51 @@ class SolutionA extends Common {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $input = $this->loadInput(__DIR__."/input.txt");
-        $solution = 0;
 
-        $this->printSolution((string) $solution);
+        $gamma = [];
+        $epsilon = [];
+
+        $zeros = [];
+        $ones = [];
+
+        foreach($input as $mask) {            
+            $array = str_split(trim($mask));
+
+            foreach($array as $key => $bit) {                
+                if((string) $bit === "0" ) {
+                    if(isset($zeros[$key])) {                        
+                        $zeros[$key] = ++$zeros[$key]; 
+                    }
+                    else {
+                        $zeros[$key] = 1;
+                    }
+                }
+
+                if((string) $bit === "1" ) {
+                    if(isset($ones[$key])) {
+                        $ones[$key] = ++$ones[$key]; 
+                    }
+                    else {
+                        $ones[$key] = 1;
+                    }
+                }
+            }                
+        }
+
+        for($i=0;$i<count($zeros);$i++) {
+            if($zeros[$i] > $ones[$i]) {
+                $gamma[$i] = 0;
+                $epsilon[$i] = 1;
+            }
+            else{
+                $gamma[$i] = 1;
+                $epsilon[$i] = 0; 
+            }
+        }
+
+        $powerConsumption = bindec(implode($gamma)) * bindec(implode($epsilon));
+
+        $this->printSolution((string) $powerConsumption);
 
         return Command::SUCCESS;
     }
